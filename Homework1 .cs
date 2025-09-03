2286f8aeb1119371433f257f5ef0e08e4cad595a
klizard1o <klizardo1@buffs.wtamu.edu>
2025-09-03 18:04:20 -0500

    Add files via upload

---
+++ Homework1 .cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int X = (int)2.5;
+        int Y = (int)3.3;
+        double Z = 4 * X * X + 3 * Y;
+        Console.WriteLine("Value of Z = " + Z);
+
+        // Equation is Z =4*X*X + 3Y, when X =2.5, Y =3.3
+    }
+}
