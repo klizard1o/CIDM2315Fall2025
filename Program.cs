468e7771b0e21a6de1fb74ddbf43166afecf938d
klizard1o <klizardo1@buffs.wtamu.edu>
2025-09-22 20:53:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Homework4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //call Q1_method()
+        int a = inputNum1;
+        int b = inputNum2;
+        LargestNumber(a, b);
+        Console.WriteLine($"The largest number is: {largest}");
+
+
+        //call Q2_method()
+    }
+    //Q1_method
+    static int LargestNumber(int a, int b) {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+
+    //Q2_method
+}
